bc82b93d73ffb68b134aa10bbb3fa6d49d1b5096
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-12-16 12:34:22 +0200

    Põhi
    16.12.2025

---
+++ TaskMate/TaskMate/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskMate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TaskMate/TaskMate/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TaskMate;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
