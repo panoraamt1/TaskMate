6274b247032ea331bb50e55029ac87e86abd9866
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2026-01-12 13:21:59 +0200

    12.01.26
    Muudatused ja parandused: HomePage.xaml ja .xaml.cs

--- TaskMate/TaskMate/Views/HomePage.xaml.cs
+++ TaskMate/TaskMate/Views/HomePage.xaml.cs
@@ -1,25 +1,16 @@
 namespace TaskMate.Views;
 
+
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+    }
 
-    private async void AddTask_Clicked(object sender, EventArgs e)
+    private async void OnAddTaskClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new AddTaskPage());
     }
-
-    private async void TasksList_Clicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new TasksListPage());
-    }
-
-    private async void TodayTasks_Clicked(object sender, EventArgs e)
-    {
-        // Võid filterdatud TasksListPage'i kasutada
-        await Navigation.PushAsync(new TasksListPage(showTodayOnly: true));
-    }
 }
+
