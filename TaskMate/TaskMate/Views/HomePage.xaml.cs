442e8de5535edd9b32e86980d172de87541231f2
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2026-01-13 12:39:40 +0200

    13.01.26
    Pea vaate taastamine

---
+++ TaskMate/TaskMate/Views/HomePage.xaml.cs
@@ -0,0 +1,17 @@
+using TaskMate.Views;
+
+namespace TaskMate.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAddTaskClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddTaskPage());
+        }
+    }
+}
