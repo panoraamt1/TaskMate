70fa0b8785ea4329e8a9acb6dcc59cf419f15325
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2026-01-13 12:02:00 +0200

    13.01.26
    Lisasin Models kausta TaskItem.cs

---
+++ TaskMate/TaskMate/Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TaskMate.Models
+{
+    class TaskItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public TimeSpan? ReminderTime { get; set; }
+        public string Priority { get; set; }
+    }
+}
