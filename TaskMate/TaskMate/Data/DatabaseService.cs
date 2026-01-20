a24892331bfda67001a6d0e5747845a662df8dd8
Opilane TTHK <opilane@tthk.ee>
2026-01-20 12:15:08 +0200

    Update DatabaseService.cs

--- TaskMate/TaskMate/Data/DatabaseService.cs
+++ TaskMate/TaskMate/Data/DatabaseService.cs
@@ -1,28 +1,46 @@
 using SQLite;
 using TaskMate.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace TaskMate.Data
 {
     public class DatabaseService
     {
+        
         private static DatabaseService _instance;
         public static DatabaseService Instance => _instance ??= new DatabaseService();
 
-        private SQLiteConnection _db;
+        
+        private readonly SQLiteAsyncConnection _database;
 
         private DatabaseService()
         {
             var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "taskmate.db3");
-            _db = new SQLiteConnection(dbPath);
-            _db.CreateTable<TaskItem>();
+
+            
+            _database = new SQLiteAsyncConnection(dbPath);
+
+           
+            _database.CreateTableAsync<TaskItem>().Wait();
         }
 
-        public List<TaskItem> GetTasks() => _db.Table<TaskItem>().ToList();
+        
+        public Task<List<TaskItem>> GetTasksAsync()
+            => _database.Table<TaskItem>().ToListAsync();
 
-        public void AddTask(TaskItem task) => _db.Insert(task);
+       
+        public Task<int> SaveTaskAsync(TaskItem task)
+        {
+            if (task.Id != 0)
+                return _database.UpdateAsync(task); 
+            else
+                return _database.InsertAsync(task); 
+        }
 
-        public void DeleteTask(int id) => _db.Delete<TaskItem>(id);
-
-        public void UpdateTask(TaskItem task) => _db.Update(task);
+        
+        public Task<int> DeleteTaskAsync(TaskItem task)
+            => _database.DeleteAsync(task);
     }
 }
